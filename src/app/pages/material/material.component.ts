f1ee5fd0d7cde2152edbc356734e5534b1b03d00
DanielGualli <dc.gualli@yavirac.edu.ec>
2023-06-15 21:06:35 -0500

    creando canbios material

--- src/app/pages/material/material.component.ts
+++ src/app/pages/material/material.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+interface Materia {
+  nombre: string;
+  codigo: string;
+}
 
 @Component({
   selector: 'app-material',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./material.component.css']
 })
 export class MaterialComponent {
+  materias: Materia[] = [
+    { nombre: 'Matemáticas', codigo: 'MAT01' },
+    { nombre: 'Historia', codigo: 'HIS01' },
+    { nombre: 'Ciencias', codigo: 'CIE01' }
+  ];
+
+  nuevaMateria: Materia = {
+    nombre: '',
+    codigo: ''
+  };
+
+  registrarMateria() {
+    this.materias.push(this.nuevaMateria);
+    this.nuevaMateria = { nombre: '', codigo: '' };
+  }
 
 }
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TeachersComponent } from './teachers/teachers.component';
 import { PagesComponent } from './pages.component';
 import { RegisterTeacherComponent } from './register-teacher/register-teacher.component';
+import { MaterialComponent } from './material/material.component';
 
 const routes: Routes = [
   {path:"pages", component: PagesComponent,
    children:[
     {path: "", component: TeachersComponent},
-    {path:'register', component: RegisterTeacherComponent}
+    {path:'register', component: RegisterTeacherComponent},
+    {path:'material', component: MaterialComponent},
    ]},
 ];
 
