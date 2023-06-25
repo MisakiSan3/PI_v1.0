3520107e659dcbeb570f9fcd8fe6da7a6e6242a7
MisakiSan3 <you@example.com>
2023-06-25 09:14:38 -0500

    Avance del login. Isak

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserAuthModel } from 'src/app/models/auth-model.entity';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   constructor(private router: Router) {
     
+  }
+  user: UserAuthModel ={
+    email: '',
+    password: ''
+  }
+  ngOnInit(): void {
+
   }
   
   navigateToRegister() {
     this.router.navigateByUrl("/register");
   }
-
+  logIn(){
+    console.log(this.user);
+    
+  }
 }
